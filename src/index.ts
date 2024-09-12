ee2cafbeecebcdc441be1828edfb47bd890f85e1
Abanoub Zaky <abanoub.zaky@caffeina.com>
2024-09-12 21:35:21 +0300

    add config env files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
+import config from "./config";
 
-import app from "./server"
+import app from "./server";
 
-app.listen(3001, () => {
-    console.log('hello on port 3001')
-})
+app.listen(config.port, () => {
+  console.log(`hello on port ${config.port}`);
+});
